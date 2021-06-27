05c02cd568cb57e24c0c05b4c33c95455ed525b7
linda400 <4a51c019@stust.edu.tw>
2021-06-27 23:32:30 +0800

    animation Egg

---
+++ dev/js/dinoGameEgg.js
@@ -0,0 +1,8 @@
+let Bt = document.getElementById("Bt");
+Bt.addEventListener('click', function (e) {
+    document.getElementById("Bt").classList.add("remove");
+    document.getElementById("Egg").classList.add("egg-animation");
+    document.getElementById("egg-1").classList.add("click");
+    document.getElementById("egg-2").classList.add("click");
+    document.getElementById("egg-3").classList.add("click");
+  })
