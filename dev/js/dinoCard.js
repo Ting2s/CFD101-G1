7d41d4363375104a38719af62a57d89179d9a1c0
mico52 <ui74vb32@yahoo.com.tw>
2021-06-27 20:36:08 +0800

    add dinoCardboard and change dinoCard sass and html

--- dev/js/dinoCard.js
+++ dev/js/dinoCard.js
@@ -1,20 +1,6 @@
-function _(selector){
-  return document.querySelector(selector);
-}
-function setup() {
-  let canvas=createCanvas(650,600);
-  canvas.parent("canvasWrapper");
-  background(255);
-}
-function mouseDragged(){
-  let type= _("#penPen").checked?"pen":"brush";
-  let size=parseInt(_("#penSize").value);
-  let color=_("#penColor").value;
-  fill(color);
-  stroke(color);
-  if(type=="pen"){
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }else{
-    ellipse(mouseX, mouseY, size, size);
-  }
-}
+LC.init(
+  document.getElementsByClassName('myDrawing')[0],
+  {imageURLPrefix: 'images/dinoCard'}
+  // LiterallyCanvas(.downLoad, getSnapshot(keys=['shapes', 'imageSize', 'colors', 'position', 'scale', 'backgroundShapes']))
+);
+
