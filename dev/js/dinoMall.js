b5c4fa8d9edb13d48a4ad638cdddac05b4c7104a
Alextw69 <lib151886@gmail.com>
2021-06-29 22:23:00 +0800

    ALEX  modified  Mall.html (dini-bg-padding)

--- dev/js/dinoMall.js
+++ dev/js/dinoMall.js
@@ -6,14 +6,26 @@
       getId("showHat").src = e.target.src;
       getId("showHat").style.display="";      
     }
+    function showCart(){
+      alert("購物系統維修中");
+    }
 
     function init(){
-      let imgs =  document.getElementsByClassName("imgClass");
+      // let imgs =  document.getElementsByClassName("imgClass");
+      let imgs =  document.querySelectorAll('.imgClass');
+      console.log(imgs);
       for(let i=0; i<imgs.length; i++){
         imgs[i].onclick = showImg;
       }
+      let cart = document.getElementsByClassName('cart')[0];
+      cart.onclick = showCart;
     }
 
     window.onload = init;
 
-  
+    // let card_btn = document.getElementById("card_btn");
+    // card_btn.addEventListener("click", function () {
+      
+    //   document.getElementById("myCard").classList.add("none");
+    
+    //   document.getElementById("card").classList.remove("none");
