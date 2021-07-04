6c1b73a26fecf10a67841ced3069939250ca1d7f
linda400 <4a51c019@stust.edu.tw>
2021-07-05 00:55:32 +0800

    game成長條 教學燈箱

--- dev/js/dinoGame.js
+++ dev/js/dinoGame.js
@@ -5,62 +5,69 @@ let Fish_3 = document.getElementById("Fish-3");
 let Fish_4 = document.getElementById("Fish-4");
 let Fish_5 = document.getElementById("Fish-5");
 let Fish_6 = document.getElementById("Fish-6");
+let i = 0;
 
 //----fish-1事件
-function hide(){
+function hide() {
     Fish_1.classList.add('remove');
+    add();
 }
-function appear(){
+function appear() {
     setInterval(() => {
         Fish_1.classList.remove('remove');
     }, 5000);
 }
 
 //----fish-2事件
-function hide_2(){
+function hide_2() {
     Fish_2.classList.add('remove');
+    add();
 }
-function appear_2(){
+function appear_2() {
     setInterval(() => {
         Fish_2.classList.remove('remove');
     }, 5000);
 }
 
 //----fish-3事件
-function hide_3(){
+function hide_3() {
     Fish_3.classList.add('remove');
+    add();
 }
-function appear_3(){
+function appear_3() {
     setInterval(() => {
         Fish_3.classList.remove('remove');
     }, 5000);
 }
 
 //----fish-4事件
-function hide_4(){
+function hide_4() {
     Fish_4.classList.add('remove');
+    add();
 }
-function appear_4(){
+function appear_4() {
     setInterval(() => {
         Fish_4.classList.remove('remove');
     }, 5000);
 }
 
 //----fish-5事件
-function hide_5(){
+function hide_5() {
     Fish_5.classList.add('remove');
+    add();
 }
-function appear_5(){
+function appear_5() {
     setInterval(() => {
         Fish_5.classList.remove('remove');
     }, 4000);
 }
 
 //----fish-6事件
-function hide_6(){
+function hide_6() {
     Fish_6.classList.add('remove');
+    add();
 }
-function appear_6(){
+function appear_6() {
     setInterval(() => {
         Fish_6.classList.remove('remove');
     }, 5000);
@@ -81,10 +88,18 @@ function init() {
     Fish_6.onclick = hide_6;
 }
 
+
+
 //grow up進度條
+let j = 0;
+let sq = document.getElementById("sq_"+j);
 
-
-
+let Grow = document.getElementsByClassName('Grow-sq')[0];
+function add(){
+        Grow.children[j].style.display = 'block';
+        j ++;
+        console.log(Grow.children[j]);
+}
 
 window.onload = init;
 
