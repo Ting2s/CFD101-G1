c5ca1cd6ea676188e3b5d7f940b46a2801adeaa1
Ting2s <istingting0@gmail.com>
2021-07-07 19:30:08 +0800

    新增首頁三個場景動態文字、修正破圖bug

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -184,18 +184,18 @@ anime.timeline({loop: true})
 
 // S2介紹文字動畫
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml9 .letters');
+var textWrapper = document.querySelector('.slogan .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml9 .letter',
+    targets: '.slogan .letter',
     scale: [0, 1],
     duration: 1500,
     elasticity: 600,
     delay: (el, i) => 45 * (i+1)
   }).add({
-    targets: '.ml9',
+    targets: '.slogan',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
