89c8a4fcaa10259e81112b1f637f5c3674692821
gabklc1592 <pyhnck5893@gmail.com>
2021-07-09 19:54:26 +0800

    add

--- dev/js/dinoDisscus.js
+++ dev/js/dinoDisscus.js
@@ -3,8 +3,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino6.jpg',
         title: '有可能用琥珀中的恐龍DNA來復活恐龍嗎？',
-        date: '2020/10/21',
-        view: '5',
+        date: '2021-07-08 12:10:12',
         author: '王小明',
         category: '文章類別 :',
         span: '恐龍知識',
@@ -13,8 +12,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino7.jpg',
         title: '恐龍世界第一話:歡樂的一天!',
-        date: '2020/6/25',
-        view: '13',
+        date: '2021-07-08 12:10:12',
         author: '王小明',
         category: '文章類別 :',
         span: '恐龍創作',
@@ -23,8 +21,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino8.jpg',
         title: '想瞭解三角龍的冷知識嗎？',
-        date: '2020/5/16',
-        view: '3',
+        date: '2021-07-08 12:10:12',
         author: '徐涵涵',
         category: '文章類別 :',
         span: '恐龍知識',
@@ -33,8 +30,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino3.jpg',
         title: '跟著劍龍去野餐',
-        date: '2020/4/27',
-        view: '6',
+        date: '2021-07-08 12:10:12',
         author: '徐涵涵',
         category: '文章類別 :',
         span: '恐龍創作',
@@ -43,8 +39,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino4.jpg',
         title: '如何養出滿血迅猛龍?分享熱騰騰的攻略',
-        date: '2020/3/21',
-        view: '7',
+        date: '2021-07-08 12:10:12',
         author: '王小明',
         category: '文章類別 :',
         span: '恐龍分享',
@@ -53,8 +48,7 @@ let data = {
       {
         imgsrc: './images/disscus/dino5.jpg',
         title: '這次拍的亮黃色劍龍!高清40P大家也來分享',
-        date: '2020/2/21',
-        view: '5',
+        date: '2021-07-08 12:10:12',
         author: '王小明',
         category: '文章類別 :',
         span: '恐龍分享',
