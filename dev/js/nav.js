b985d746435dce393c7f9d83ad10820463af2dc8
linda400 <4a51c019@stust.edu.tw>
2021-07-17 20:17:23 +0800

    登入會員完成

--- dev/js/nav.js
+++ dev/js/nav.js
@@ -10,3 +10,19 @@ Avatar.addEventListener('mouseleave', () => {
 
 //會員箱
 
+let Avatar_box = document.querySelector('.Avatar-box');
+let isShowSlide = false;
+Avatar.addEventListener('click', function (e) {
+    isShowSlide =! isShowSlide
+    if (isShowSlide) {
+        console.log(true);
+        Avatar_box.classList.remove("remove");
+        Avatar_box.classList.add("show");
+        // document.querySelector(".")
+      
+    } else {
+        console.log(false);
+        Avatar_box.classList.add("remove"); 
+        Avatar_box.classList.remove("show");    
+    }
+}, false);
