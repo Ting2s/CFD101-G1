daa7089a4ebdaa6a32c44b7e834d7a90c93db276
ailer8469 <ailer8469@gmail.com>
2021-07-19 21:20:49 +0800

    test更改

--- dev/js/dinoTest.js
+++ dev/js/dinoTest.js
@@ -3,21 +3,12 @@ new Vue({
 
   el: ('#app'),
   data: {
-    answers:
-      [{
-        name: "A",
-        option: ""
-      }
-      ,{name:"B",option:""}
-      ,{name:"C",option:""}
-      ,{name:"D",option:""}
-      ],
     //題庫
     questions:[],
 //============== 
-    isClick:false,   
+    isClick:'clicked',
 //==============   
-    isRight: true,
+
     isShow: false,
 //==============
     isStart: false,
@@ -33,58 +24,150 @@ new Vue({
     comment_C: "再加油！",
 //對錯===========
     rightImg: "images/dinoTest/pic/right.png",
-    wrongImg: "images/dinoTest/pic/x.png"
-  },
-  computed: {
-
-
-    choose(i) {
-
-      this.isShow = true;
-      this.isClick = true;
-    //顯示對錯
-      if (this.clicked = true &&
-        this.questions[i].target.value == this.questions[i].quiz_a) {
-        this.wrongImg = false;
-        this.rightImg = true;
-      } else {
-        this.wrongImg = true;
-        this.rightImg = false;
-      }
-    },
-    //當選取的按鈕和quiz_a相同時->加一分
-    rightPoint() {
-      if (this.clicked = true &&
-        this.questions[i].target.value == this.questions[i].quiz_a) {
-        return this.point = 1;
-      } else {
-        return this.point = 0;
-      }
-    },
-
-    //對錯計算
-    finalScore() {
-      for (let i = 1; i<=7; i++) {
-        return this.allPoint += this.rightPoint();
-      };
-    },
+    wrongImg: "images/dinoTest/pic/x.png",
+//對錯===========
+    isRight_1: false,
+    isWrong_1: false,
+    isRight_2: false,
+    isWrong_2: false,
+    isRight_3: false,
+    isWrong_3: false,
+    isRight_4: false,
+    isWrong_4: false,
+//點擊===========
+    choose_1: false,
+    choose_2: false,
+    choose_3: false,
+    choose_4:false,
   },
 
   methods: {
+    init() {
+        this.isRight_1 = false;
+        this.isWrong_1 = false;
+        this.isRight_2 = false;
+        this.isWrong_2 = false;
+        this.isRight_3 = false;
+        this.isWrong_3 = false;
+        this.isRight_4 = false;
+        this.isWrong_4 = false;
+    },
+    //判定
+    see_1(i) {
+      this.init();
+      this.isShow = true;
+    //亮燈
+      this.choose_1 = true;
+    //顯示對錯
+      if (this.questions[i].quiz_opt1 == this.questions[i].quiz_a) {
+        // console.log(i);
+        this.isRight_1 = true;
+        this.isWrong_2 = true;
+        this.isWrong_3 = true;
+        this.isWrong_4 = true;
+      }
+      // } else if (this.questions[i].quiz_opt2 == this.questions[i].quiz_a) {
+      //   this.isRight_2 = true;
+      //   this.isWrong_1 = true;
+      //   this.isWrong_3 = true;
+      //   this.isWrong_4 = true;
+      // } else if (this.questions[i].quiz_opt3 == this.questions[i].quiz_a) {
+      //   this.isRight_3 = true;
+      //   this.isWrong_1 = true;
+      //   this.isWrong_2 = true;
+      //   this.isWrong_4 = true;
+      // } else if (this.questions[i].quiz_opt4 == this.questions[i].quiz_a) {
+      //   this.isRight_4 = true;
+      //   this.isWrong_1 = true;
+      //   this.isWrong_2 = true;
+      //   this.isWrong_3 = true;
+      // }
+    },
+
+    see_2(i) {
+      this.init();
+      this.isShow = true;
+    //亮燈
+      this.choose_2 = true;
+    //顯示對錯
+    if (this.questions[i].quiz_opt2 == this.questions[i].quiz_a) {
+      this.isRight_2 = true;
+      this.isWrong_1 = true;
+      this.isWrong_3 = true;
+      this.isWrong_4 = true;
+    } else {
+      this.isWrong_2 = true;
+    }
+    },
+
+    see_3(i) {
+      this.init();
+      this.isShow = true;
+    //亮燈
+      this.choose_3 = true;
+    //顯示對錯
+    if (this.questions[i].quiz_opt3 == this.questions[i].quiz_a) {
+      this.isRight_3 = true;
+      this.isWrong_1 = true;
+      this.isWrong_2 = true;
+      this.isWrong_4 = true;
+    } else {
+      this.isWrong_3 = true;
+    }
+    },
+
+    see_4(i) {
+    this.init();
+    this.isShow = true;
+    //亮燈
+    this.choose_4 = true;
+    //顯示對錯
+    if (this.questions[i].quiz_opt4 == this.questions[i].quiz_a) {
+      this.isRight_4 = true;
+      this.isWrong_1 = true;
+      this.isWrong_2 = true;
+      this.isWrong_3 = true;
+    } else {
+      this.isWrong_4 = true;
+    }
+    },
+     //當選取的按鈕和quiz_a相同時->加一分
+    rightPoint(i) {
+      if (this.choose_1 == true &&
+        this.questions[i].quiz_opt1 == this.questions[i].quiz_a) {
+        this.point = 1;
+      } else {
+        this.point = 0;
+      }
+
+      if (this.choose_2 == true &&
+        this.questions[i].quiz_opt2 == this.questions[i].quiz_a) {
+        this.point = 1;
+      } else {
+        this.point = 0;
+      }
+
+      if (this.choose_3 == true &&
+        this.questions[i].quiz_opt3 == this.questions[i].quiz_a) {
+        this.point = 1;
+      } else {
+        this.point = 0;
+      }
+
+      if (this.choose_4 == true &&
+        this.questions[i].quiz_opt4 == this.questions[i].quiz_a) {
+        this.point = 1;
+      } else {
+        this.point = 0;
+      }
+    },
     //遊戲開始*
     game() {
       this.isStart = true;
       this.isGame = false;
+      this.isShow = false;
       this.questions[0].status = 1;
     },
-
-    //轉換選項
-    options(i) {
-      //this.answers[i].option = `${this.questions[i]}` + `.quiz_${i + 1}`;
-      
-    
-    },
-
 
     //撈資料庫*
     myQuiz(){
@@ -100,50 +183,51 @@ new Vue({
       xhr.open("get", "./php/getQuestion.php", true);
       xhr.send(null);
     },
-  
-    //遊玩結果讀入資料庫
-    readInRank() {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function(){
-        if (xhr.status == 200) {
-          //存進後台
-        }else{
-          alert(xhr.status);
-        }
-      }
-      xhr.open("get", "./php/insertRank.php", true);
-      xhr.send(null);
-    },
-
-    //切換下頁鍵
+    //切換下頁鍵*
     nextTitle(i) {
-      console.log(this.questions[6].status);
+      
       if (this.questions[i].status < this.questions[i + 1].status) {
-        this.isClick = false;
+        this.choose_1 = false;
+        this.choose_2 = false;
+        this.choose_3 = false;
+        this.choose_4 = false;
         this.isShow = false;
+        
         this.questions[i].status = 2;
         this.questions[i + 1].status = 1;
       }
     else if(this.questions[i].status > this.questions[i + 1].status) {
-    this.isClick = false;
-    this.isShow = false;
-    this.questions[i + 1].status = 2;
-    this.questions[i + 2].status = 1;
+        this.choose_1 = false;
+        this.choose_2 = false;
+        this.choose_3 = false;
+        this.choose_4 = false;
+        this.isShow = false;
+        
+        this.questions[i + 1].status = 2;
+        this.questions[i + 2].status = 1;
     }
     //切換成分數頁(scoreBoard) 
-      else if (this.questions[6].status ==1) { 
+    if (this.questions[7].status ==1) { 
         this.isGame = true;
         this.isScore = false;
         this.isWrapper = true;
     }else{} 
 
-
+    // if(this.isClick_1 ==false && this.isClick_2 ==false &&
+    //   this.isClick_3 ==false && this.isClick_4 == false) {
+    //   alert("嘿！你還有題目沒答喔！");
+    //   return;
+      
+    //   }
     },
 
 
     //重新開始*
     restart() {
-      this.isClick = false;
+      this.choose_1 = false;
+      this.choose_2 = false;
+      this.choose_3 = false;
+      this.choose_4 = false;
       this.isShow = false;
 
       this.questions[0].status = 1
@@ -157,6 +241,11 @@ new Vue({
       this.isScore = true;
       this.isGame = false;
       this.isWrapper = false;
+
+      this.choose_1 = false;
+      this.choose_2 = false;
+      this.choose_3 = false;
+      this.choose_4 = false;
 
       this.questions[0].status = 1
       for (let i = 1; i < this.questions.length; i++) {
@@ -174,8 +263,16 @@ new Vue({
         return this.comment_C;
       }
     }
-
   },
+
+  computed: {
+    //對錯計算
+      finalScore() {
+        for (let i = 1; i<=7; i++) {
+          return this.allPoint += this.rightPoint(index);
+        };
+      },
+    },
   
   
   
@@ -183,3 +280,30 @@ new Vue({
     this.myQuiz();
   }
 })
+
+//==========================================
+function $id(id) {
+  return document.getElementById(id);
+}
+function insertRank() {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+    //存進後台
+    }else {
+    alert(xhr.status);
+  }
+  }
+
+  xhr.open("post", "./php/insertRank.php", true);
+  xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+  
+  let data_info = `score=${$id("finalScore").value}`;
+  xhr.send(data_info);
+}
+
+
+function init() {
+  insertRank();
+}; 
+window.onload = init;
