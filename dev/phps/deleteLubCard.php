9d34c4c40bd0a7fea1d7a6c19e4496c866d60da2
ailer8469 <ailer8469@gmail.com>
2021-07-21 22:28:58 +0800

    刪除明信片

--- dev/phps/deleteLubCard.php
+++ dev/phps/deleteLubCard.php
@@ -3,16 +3,16 @@ ob_start();
 session_start();
 
 $mem_no=$_SESSION['mem_no'];
-echo $_POST['card_no'];
 
 try{
   require_once("../connect_cfd101g1.php");
   
-  $sql = "DELETE FROM `card`
-  WHERE `card`.`card_no` = :card_no";
+  $sql = "DELETE FROM `card_favorite` 
+  WHERE `card_favorite`.`mem_no` = :mem_no 
+  AND `card_favorite`.`card_no` = :card_no;";
 
   $newPhoto = $pdo->prepare($sql);
-  // $newPhoto->bindValue(":mem_no", $mem_no);
+  $newPhoto->bindValue(":mem_no", $mem_no);
   $newPhoto->bindValue(":card_no", $_POST['card_no']);
   $newPhoto->execute();  
 
