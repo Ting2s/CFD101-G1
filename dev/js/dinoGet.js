1b070b9f65881455e6e2ac1330a0ff000a584357
linda400 <4a51c019@stust.edu.tw>
2021-07-24 01:14:51 +0800

    調整會員飼養恐龍

--- dev/js/dinoGet.js
+++ dev/js/dinoGet.js
@@ -6,7 +6,7 @@ function get() {
 function initElement() {
     console.log('in');
     let getDino = document.getElementById("getDinoSea");
-    getDino.onclick = function(){debugger;
+    getDino.onclick = function(){
         let xhr = new XMLHttpRequest();
             xhr.onload = function () {
               if (xhr.status == 200) {
