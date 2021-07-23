ca6fe8f0d63bc60cfab2df949090a1bca4ad1ddc
mico52 <ui74vb32@yahoo.com.tw>
2021-07-23 09:01:59 +0800

    modify two pages

--- dev/phps/getCard.php
+++ dev/phps/getCard.php
@@ -2,7 +2,7 @@
 try{
 	require_once("../connect_cfd101g1.php");
 	// $sql = "SELECT * FROM card c join card_favorite f on c.card_no = f.card_no WHERE = m.mem_no";
-  $sql = "SELECT a.*,m.mem_id FROM `member` m join card a WHERE a.mem_no=m.mem_no order by a.card_no";
+  $sql = "SELECT a.*,m.mem_id, m.mem_nicname FROM `member` m join card a WHERE a.mem_no=m.mem_no order by a.card_no";
   // $sql = "SELECT a.*,m.mem_id FROM `member` m join card_favorite a WHERE a.mem_no=m.mem_no";
 	//$mem = $_session['mem_no']; 
 	//$card = $pdo->query($sql);
