a4cce53b6c214cbc015d415d9c1459df6e33a3a9
mico52 <ui74vb32@yahoo.com.tw>
2021-07-23 23:11:52 +0800

    fini dinoCard

--- dev/phps/makeCard.php
+++ dev/phps/makeCard.php
@@ -4,7 +4,6 @@
 
 try{
     require_once("../connect_cfd101g1.php");
-
     // if(isset($_SESSION["mem_no"])){ //已登入
     //   $result = [
     //     "status"=>"N", 
@@ -12,20 +11,28 @@ try{
     //   ];
     //   echo  json_encode($result);
     // };
-
+    // `./images/dinoCardboard/${fileName.jpg`
+    $upload_dir = "../images/dinoCardboard/";
+    if(! file_exists($upload_dir)){
+      mkdir($upload_dir);
+    }
 
     $img = $_POST['hidden_data'];
-    //$img = str_replace('data:image/png;base64,', '', $img);
+    $img = str_replace('data:image/png;base64,', '', $img);
     $img = str_replace('', '+', $img);
-    $data = base64_decode($img);
-    $fileName = date("Ymd");
-    $file = $fileName; //  20210709
-    $success = file_put_contents($file, $data);
     
-    $sql = "INSERT INTO `card` (`card_no`, `card_img`,`card_status`,`mem_no`) VALUES (null,$file,0,1)";
-    $dino = $pdo->exec($sql);
+    $data = base64_decode($img);
+    $fileName = time().".png";
+    $file = $upload_dir.$fileName;
+    $success = file_put_contents($file, $data);
+    $file2 = substr($file,1);
 
-   }catch(PDOException $e){
+    $sql = "INSERT INTO `card` (`card_no`, `card_img`,`card_status`,`mem_no`) VALUES (null,'$file2',0,1)";
+    $dino = $pdo->exec($sql);
+    echo $file2;
+
+   }catch(PDOException $e){ 
     echo $e->getMessage();
+    
   }
 ?>
